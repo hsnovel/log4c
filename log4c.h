b6a27105f374210449a8a7fcb55e13730ca4d166
xcatalyst <cagankorkmaz35@gmail.com>
2023-02-11 02:06:49 +0300

    LOG4C_RELEASE mod added
    If the new mode is enabled log4c does not print line numbers, date
    etc.. Only the printf arguments are printed as if they are just error codes

--- log4c.h
+++ log4c.h
@@ -23,6 +23,7 @@
  */
 
 #ifndef LOG4C_DISABLE
+
 #define log_notag(...) _log(LOG_NOTAG, __LINE__, __FILE__, __VA_ARGS__)
 #define log_ok(...) _log(LOG_OK, __LINE__, __FILE__, __VA_ARGS__)
 #define log_trace(...) _log(LOG_TRACE, __LINE__, __FILE__, __VA_ARGS__)
@@ -31,6 +32,15 @@
 #define log_warn(...)  _log(LOG_WARN, __LINE__, __FILE__, __VA_ARGS__)
 #define log_error(...) _log(LOG_ERROR, __LINE__, __FILE__, __VA_ARGS__)
 #define log_fatal(...) _log(LOG_FATAL, __LINE__, __FILE__, __VA_ARGS__)
+
+#elif defined(LOG4C_RELEASE)
+
+#define log_notag(...)
+#define log_ok(...)
+#define log_trace(...)
+#define log_debug(...)
+#define log_info(...)
+#define log_warn(...)
 
 #else
 
@@ -45,9 +55,11 @@
 
 #endif
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/*
+ * #ifdef __cplusplus
+ * extern "C" {
+ * #endif
+ */
 
 enum { LOG_NOTAG, LOG_OK, LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
@@ -70,9 +82,11 @@ static struct {
 	.thread_safe_initialized = 0,
 };
 
+#if !defined LOG4C_RELEASE
 static const char* _log_level_strings[] = {
 	"NOTAG", "OK", "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"
 };
+#endif
 
 #if !defined(LOG4C_DISABLE_COLOR)
 #if defined(LOG4C_ENABLE_BOLD_COLORS)
@@ -80,9 +94,11 @@ static const char* _log_level_colors[] = {
 	"\x1b[1;39m", "\x1b[1;32m", "\x1b[1;35m", "\x1b[1;34m", "\x1b[1;32m", "\x1b[1;33m", "\x1b[1;31m", "\x1b[1;31m"
 };
 #else
+#if !defined(LOG4C_RELEASE)
 static const char* _log_level_colors[] = {
 	"\x1b[39m", "\x1b[32m", "\x1b[35m", "\x1b[34m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[31m"
 };
+#endif
 #endif
 #endif
 
@@ -135,8 +151,9 @@ inline static void log_detach_fp()
 	_log_global_settings.file_descriptor = NULL;
 }
 
-static void _log(int level, int line, const char* file_name, const char* args, ...)
+inline static void _log_handle_config(int level, int *scoped_level)
 {
+	/* Mutex */
 	if (_log_global_settings.thread_safe_initialized) {
 #ifdef _MSC_VER
 		EnterCriticalSection(&_log_global_settings.mutex);
@@ -145,73 +162,71 @@ static void _log(int level, int line, const char* file_name, const char* args, .
 #endif
 	}
 
-	int scoped_level;
-	struct tm* current_time;
-	va_list variadic_list;
-
+	/* Quiet Mode */
 	if (_log_global_settings.quiet == 0)
 		_log_global_settings.terminal_descriptor = stderr;
 	else
 		_log_global_settings.terminal_descriptor = stdout;
 
+	/* Min Level */
 	if (level >= _log_global_settings.level)
-		scoped_level = level;
+		*scoped_level = level;
 	else
-		scoped_level = _log_global_settings.level;
+		*scoped_level = _log_global_settings.level;
+}
 
-#ifdef _MSC_VER
-#else
+#if !defined (_MSC_VER)
+static void _log(int level, int line, const char* file_name, const char* args, ...)
+{
+	int scoped_level;
+	struct tm* current_time;
+	va_list variadic_list;
+
+	_log_handle_config(level, &scoped_level);
+
 	time_t tx = time(NULL);
 	current_time = localtime(&tx);
-#endif
 
-	char buf[16];
+	char date[16];
 
 	va_start(variadic_list, args);
-#ifdef _MSC_VER
-	SYSTEMTIME LocalTime;
-	GetLocalTime(&LocalTime);
-#else
-	buf[strftime(buf, sizeof(buf), "%H:%M:%S", current_time)] = '\0';
-#endif
+	date[strftime(date, sizeof(date), "%H:%M:%S", current_time)] = '\0';
 
+#ifdef LOG4C_RELEASE
+
+#else
 #if !defined(LOG4C_DISABLE_COLOR)
-#ifdef _MSC_VER
-	fprintf(_log_global_settings.terminal_descriptor, "%d:%d:%d %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m",
-		LocalTime.wHour, LocalTime.wMinute, LocalTime.wSecond, _log_level_colors[scoped_level], _log_level_strings[scoped_level], file_name, line);
-#else
 	fprintf(_log_global_settings.terminal_descriptor, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m",
-		buf, _log_level_colors[scoped_level], _log_level_strings[scoped_level], file_name, line);
-#endif
+		date, _log_level_colors[scoped_level], _log_level_strings[scoped_level], file_name, line);
 #else
-	fprintf(_log_global_settings.terminal_descriptor, "%s %-5s %s:%d ", buf, _log_level_strings[scoped_level],
+	fprintf(_log_global_settings.terminal_descriptor, "%s %-5s %s:%d ", date, _log_level_strings[scoped_level],
 		file_name, line);
 #endif
+#endif
 
+	/* Print the string */
+	vfprintf(_log_global_settings.terminal_descriptor, args, variadic_list);
+	putc('\n', _log_global_settings.terminal_descriptor);
+
+#if defined LOG4C_RELEASE
+#else
 	if (_log_global_settings.file_descriptor) {
 		va_list file_list;
 		va_copy(file_list, variadic_list);
-#ifdef _MSC_VER
-		fprintf(_log_global_settings.file_descriptor, "%d:%d:%d %-5s %s:%d: ",
-			LocalTime.wHour, LocalTime.wMinute, LocalTime.wSecond, _log_level_strings[scoped_level], file_name, line);
-#else
+
 		fprintf(_log_global_settings.file_descriptor, "%s %-5s %s:%d: ",
-			buf, _log_level_strings[scoped_level], file_name, line);
-#endif
+			date, _log_level_strings[scoped_level], file_name, line);
+
 		vfprintf(_log_global_settings.file_descriptor, args, file_list);
 		putc('\n', _log_global_settings.file_descriptor);
 		fflush(_log_global_settings.file_descriptor);
 	}
-
-
-	/* Print to terminal */
-	vfprintf(_log_global_settings.terminal_descriptor, args, variadic_list);
-	putc('\n', _log_global_settings.terminal_descriptor);
+#endif
 
 	/*
-	 * Normaly terminals are line buffered but we obviously:
+	 * Normaly terminals are line datefered but we obviously:
 	 *
-	 *   - cannot rely on that as stream buffering mode might be
+	 *   - cannot rely on that as stream datefering mode might be
 	 *     set to something different by the programmer
 	 *   - cannot guarantee that the terminal will flush on a new line
 	 *
@@ -222,16 +237,67 @@ static void _log(int level, int line, const char* file_name, const char* args, .
 
 	va_end(variadic_list);
 	if (_log_global_settings.thread_safe_initialized) {
-#ifdef _MSC_VER
-		LeaveCriticalSection(&_log_global_settings.mutex);
-#else
 		pthread_mutex_unlock(&_log_global_settings.mutex);
+	}
+}
+
+#else
+
+static void _log(int level, int line, const char* file_name, const char* args, ...)
+{
+	int scoped_level;
+	struct tm* current_time;
+	va_list variadic_list;
+
+	_log_handle_config(level, &scoped_level);
+
+	va_start(variadic_list, args);
+	SYSTEMTIME LocalTime;
+	GetLocalTime(&LocalTime);
+
+
+#if !defined(LOG4C_DISABLE_COLOR)
+	fprintf(_log_global_settings.terminal_descriptor, "%d:%d:%d %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m",
+		LocalTime.wHour, LocalTime.wMinute, LocalTime.wSecond, _log_level_colors[scoped_level], _log_level_strings[scoped_level], file_name, line);
+#else
+	fprintf(_log_global_settings.terminal_descriptor, "%s %-5s %s:%d ", date, _log_level_strings[scoped_level],
+		file_name, line);
 #endif
+
+	/* Print string */
+#ifdef LOG4C_RELEASE
+#else
+	vfprintf(_log_global_settings.terminal_descriptor, args, variadic_list);
+	putc('\n', _log_global_settings.terminal_descriptor);
+
+	fflush(_log_global_settings.terminal_descriptor);
+#endif
+
+#if defined LOG4C_RELEASE
+#else
+	if (_log_global_settings.file_descriptor) {
+		va_list file_list;
+		va_copy(file_list, variadic_list);
+		fprintf(_log_global_settings.file_descriptor, "%d:%d:%d %-5s %s:%d: ",
+			LocalTime.wHour, LocalTime.wMinute, LocalTime.wSecond, _log_level_strings[scoped_level], file_name, line);
+		vfprintf(_log_global_settings.file_descriptor, args, file_list);
+		putc('\n', _log_global_settings.file_descriptor);
+		fflush(_log_global_settings.file_descriptor);
+	}
+#endif
+
+	va_end(variadic_list);
+	if (_log_global_settings.thread_safe_initialized) {
+		LeaveCriticalSection(&_log_global_settings.mutex);
 	}
 
 }
 
-#ifdef __cplusplus
-}
 #endif
+
+/*
+ * #ifdef __cplusplus
+ * }
+ * #endif
+ */
 #endif
